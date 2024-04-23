1559b2447074e37fd3a91b721ab802a69727c6ce
Ju T <53004817+JulienT01@users.noreply.github.com>
2024-04-23 15:22:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ packages = find_packages(exclude=["docs"])
 # Base installation (interface only)
 #
 install_requires = [
-    "rlberry@git+https://github.com/rlberry-py/rlberry",
+    "rlberry",
     # "numba",
     # "optuna",
     # "ffmpeg-python",
